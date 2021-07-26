4a0b5658225bdaf628334e3a7bc0c4d7dfd5592e
Greg Menounos <gmenounos@gmail.com>
2021-07-26 17:57:46 -0400

    Added product version to zip file names.

--- Publish.ps1
+++ Publish.ps1
@@ -5,16 +5,23 @@ dotnet publish kw1281test.csproj /p:PublishProfile=LinuxArm
 $PublishSourcDir = 'C:\Users\gmeno\src\kw1281test\bin\Release\net5.0\publish'
 $GitHubDir = 'C:\Users\gmeno\src\kw1281test\GitHub'
 
-Compress-Archive -Force -Path "$PublishSourcDir\Win\kw1281test.exe" -DestinationPath "$GitHubDir\kw1281test_Win10.zip"
+Remove-Item -Path $GitHubDir\*.*
 
+$WinExe = "$PublishSourcDir\Win\kw1281test.exe"
+$Version = (Get-Item $WinExe).VersionInfo.ProductVersion
+
+Compress-Archive -Force -Path $WinExe -DestinationPath "$GitHubDir\kw1281test_$($Version)_Win10.zip"
+
+$MacZip = "kw1281test_$($Version)_macOS.zip"
 Push-Location -Path "$PublishSourcDir\Mac\"
-wsl zip kw1281test_macOS.zip kw1281test
-Move-Item -Force -Path kw1281test_macOS.zip -Destination "$GitHubDir\"
+wsl zip $MacZip kw1281test
+Move-Item -Force -Path $MacZip -Destination "$GitHubDir\"
 Pop-Location
 
+$LinuxZip = "kw1281test_$($Version)_LinuxArm.zip"
 Push-Location -Path "$PublishSourcDir\LinuxArm\"
-wsl zip kw1281test_LinuxArm.zip kw1281test
-Move-Item -Force -Path kw1281test_LinuxArm.zip -Destination "$GitHubDir\"
+wsl zip $LinuxZip kw1281test
+Move-Item -Force -Path $LinuxZip -Destination "$GitHubDir\"
 Pop-Location
 
 Start-Process .\GitHub
