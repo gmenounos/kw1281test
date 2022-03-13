d00cce9a15f49acd74fa4500d402e8c01c82c026
Greg Menounos <gmenounos@gmail.com>
2022-03-13 18:33:10 -0400

    Added additional BOO logins.

--- Cluster/MotometerBOOCluster.cs
+++ Cluster/MotometerBOOCluster.cs
@@ -9,14 +9,9 @@ namespace BitFab.KW1281Test.Cluster
         public void UnlockForEepromReadWrite()
         {
             string softwareVersion = GetClusterInfo();
-            ushort login;
-            if (softwareVersion.StartsWith("h9340"))
+            if (softwareVersion.Length < 10 ||
+                !VersionToLogin.TryGetValue(softwareVersion[..10], out ushort login))
             {
-                login = 11899;
-            }
-            else
-            {
-                // TODO: Add more logins for the various BOO software versions
                 Log.WriteLine("Warning: Unknown software version. Login may fail.");
                 login = 11899;
             }
@@ -29,6 +24,37 @@ namespace BitFab.KW1281Test.Cluster
             _kwp1281.SendBlock(new List<byte> { 0x1B, 0x08, 0x15 });
             _ = _kwp1281.ReceiveBlocks();
         }
+
+        private readonly Dictionary<string, ushort> VersionToLogin = new()
+        {
+            { "a0prj008.1", 10164 },
+            { "A0prj008.2", 10164 },
+            { "a4prj010.1", 21597 },
+            { "a4prj012.1", 21597 },
+            { "h1340_05.2", 21701 },
+            { "h1340_06.2", 21601 },
+            { "h9340_08.1", 11899 },
+            { "h9340_08.2", 11899 },
+            { "h9340_09.1", 11899 },
+            { "h9340_10.1", 11899 },
+            { "h9340_10.2", 11899 },
+            { "h9340_10.3", 11899 },
+            { "h9340_11.2", 11899 },
+            { "se110_05.2", 13473 },
+            { "v9119_07.1", 19126 },
+            { "v9119_07.3", 11064 },
+            { "v9230_03.1", 10501 },
+            { "v9230_03.2", 10501 },
+            { "v9230_05.1", 44479 },
+            { "v9230_05.2", 44479 },
+            { "v9230_06.3", 23775 },
+            { "v9230_07.2", 10164 },
+            { "v9230_08.1", 10164 },
+            { "v9230_08.2", 10164 },
+            { "vw110_04.2", 08721 },
+            { "VW230_06.1", 47165 },
+            { "vw340_07.2", 05555 },
+        };
 
         public string DumpEeprom(
             uint? optionalAddress, uint? optionalLength, string? optionalFileName)
